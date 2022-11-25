3e2d18ed271e22625c083233bfb2b9c4bb4f395b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-11-25 20:14:22 +0700

    add steps in groovy function

--- src/programmerzamannow/jenkins/Output.groovy
+++ src/programmerzamannow/jenkins/Output.groovy
@@ -2,8 +2,8 @@ package programmerzamannow.jenkins;
 
 class Output {
 
-  static def hello(String name) {
-    println("Hello ${name}")
+  static def hello(steps, String name) {
+    steps.echo("Hello ${name}")
   }
 
 }
